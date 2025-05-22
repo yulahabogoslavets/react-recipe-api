6a1f04512c01a9b6ff0d7ee7d0b50ee6184189d5
yulahabogoslavets <test@test.email>
2025-05-22 14:07:45 +0200

    fix: remove slash from  base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: '/react-recipe-api/',
+    base: '/react-recipe-api',
     build: {
         rollupOptions: {
             output: {
