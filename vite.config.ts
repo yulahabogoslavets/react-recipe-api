d460958a9401a29a678f3da50f6d77cb71e36aca
yulahabogoslavets <test@test.email>
2025-05-22 13:42:08 +0200

    fix: add base to the vite config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import viteCompression from 'vite-plugin-compression'
 
-
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss(), viteCompression()],
+    base: '/react-recipe-api/',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ['react', 'react-dom'],
+                },
+            },
+        },
+        chunkSizeWarningLimit: 1000,
+    },
 })
