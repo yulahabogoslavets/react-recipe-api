4c159c7ad61aa1949349dbad81273f45986e40cd
yulahabogoslavets <test@test.email>
2025-05-22 13:48:05 +0200

    fix: remove  "vite-plugin-compression"

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import viteCompression from 'vite-plugin-compression'
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), viteCompression()],
+    plugins: [react(), tailwindcss()],
     base: '/react-recipe-api/',
     build: {
         rollupOptions: {
