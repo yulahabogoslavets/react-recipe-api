57c893f987c3d792b4cbcaa8c600100e191387f7
yulahabogoslavets <test@test.email>
2025-05-22 13:25:19 +0200

    feat: install compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteCompression from 'vite-plugin-compression'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), viteCompression()],
 })
